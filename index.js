7e2fdb03215694dcde30acc09b8359c777749856
unimatrix2 <dnny.nis@gmail.com>
2024-09-13 15:42:55 -0300

    feat: bootstrap project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
- * @type {Array<number>} array para receber os números fatorados e prover uma condição de parada
+ * @type {Array<number>} array para receber os números fatorados
  */
-const stopArray = [];
+const factoredArray = [];
 
 /**
  * @type {Array<number>} array de referência para a condição de parada
@@ -9,17 +9,30 @@ const stopArray = [];
 const stopReference = [4, 2, 1];
 
 /**
+ * @type {number} variável para rastrear o comprimento do array factoredArray
+ */
+let factoredArrayLength = 0;
+
+/**
  * 
  * @param {number} int O número a ser fatorado no problema
+ * @returns {threeX | string} Retorna uma chamada da função threeX ou a string com o array factoredArray
  */
 export default function threeX(int) {
   if(!Number.isSafeInteger(int)) throw new Error('Integer out of range');
-  if (stopArray.slice(-3).join('') === '421') return console.log(stopArray);
+  if (factoredArrayLength >= 3 && 
+    factoredArray[factoredArrayLength - 3] === stopReference[0] &&
+    factoredArray[factoredArrayLength - 2] === stopReference[1] &&
+    factoredArray[factoredArrayLength - 1] === stopReference[2]) {
+  return console.log(JSON.stringify(factoredArray));
+}
   if (int % 2 === 0) {
-    stopArray.push(int);
+    factoredArray.push(int);
+    factoredArrayLength++;
     return threeX(int / 2)
   }
-  stopArray.push(int);
+  factoredArray.push(int);
+  factoredArrayLength++;
   return threeX((int * 3) +1);
 }
 
