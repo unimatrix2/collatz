cbbb05a5b34103306c48d2b51475686fe1ecb136
unimatrix2 <dnny.nis@gmail.com>
2024-09-14 18:14:26 -0300

    fix: remove externalizing deps

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['d3-zoom', 'd3-selection', 'd3-force', 'd3-scale', 'd3-scale-chromatic', 'd3-drag', 'd3-array', 'd3-shape', 'd3-interpolate', 'd3-color', 'd3-ease', 'd3-timer', 'd3-dispatch', 'd3-transition', 'd3-hierarchy', 'd3-path', 'd3-quadtree', 'd3-random', 'd3-format', 'd3-time']
-    }
-  }
 
 })
